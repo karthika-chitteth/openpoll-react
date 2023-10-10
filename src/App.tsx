becf0dba5197e068f68a1f94b6768c4ca4505959
Prasanth NV <pnellikkaparambil@opentrends.net>
2023-10-10 15:11:27 +0530

    added basic router

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,25 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import UserHome from './modules/users/pages/home'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/user",
+    element: <UserHome />,
+  },
+]);
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <button className="btn">Hello daisyUI</button>
+      <RouterProvider router={router} />
     </>
   )
 }
