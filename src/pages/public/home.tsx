8118f3719baec4636a2be2f13e72eadd40bc55b7
karthikac <karthikac@opentrends.net>
2023-10-16 14:47:04 +0530

    home

--- src/pages/public/home.tsx
+++ src/pages/public/home.tsx
@@ -1,7 +1,17 @@
+import BrandHeader from "../../components/brand-header";
+import { Features } from "../../components/features";
+import { Footer } from "../../components/footer";
+import { HeroBanner } from "../../components/hero";
+
 export const Home = () => {
-  return (
-    <>
-     
-    </>
-  );
+return (
+<>
+  <div className="bg-[#0F172A]">
+    <BrandHeader />
+    <HeroBanner />
+    <Features />
+    <Footer />
+  </div>
+</>
+);
 };
