ff740d24ccb94f4e8a75cd7085354e3acbf42c9e
karthikac <karthikac@opentrends.net>
2023-10-16 12:35:29 +0530

    Login UI

---
+++ src/pages/auth/signin.tsx
@@ -0,0 +1,15 @@
+
+import BrandHeader from "../../components/brand-header";
+import { Login } from "../../components/forms/login";
+
+export const SignIn = () => {
+    return (
+      <>
+      <div className="bg-[#1F1C46] h-[100vh]">
+       <BrandHeader></BrandHeader>
+       <Login></Login>
+       </div>
+      </>
+    );
+  };
+  
